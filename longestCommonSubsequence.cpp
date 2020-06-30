4a83e56f2ca97ca7b45b4f40514ad262e5444dab
prakash besra <psbesra@gmail.com>
2020-07-01 01:15:53 +0530

    main added

--- longestCommonSubsequence.cpp
+++ longestCommonSubsequence.cpp
@@ -1,4 +1,11 @@
 // This is dynamic programming to solve longest common sequence problem.
 // Given two sequence X and Y, find Z, a common subsequence of X and Y
 // Reference: Introduction to Algorithms
+// Programming language: C++
 // Author: Prakash Besra
+
+#include <iostream>
+using namespace std;
+int main(){
+  return(0);
+}
