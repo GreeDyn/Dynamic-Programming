0229c5c908143d5db1bd7a1dd994439d227246e4
prakash besra <psbesra@gmail.com>
2020-07-01 01:14:17 +0530

    longest common subsequence code created

---
+++ longestCommonSubsequence.cpp
@@ -0,0 +1,4 @@
+// This is dynamic programming to solve longest common sequence problem.
+// Given two sequence X and Y, find Z, a common subsequence of X and Y
+// Reference: Introduction to Algorithms
+// Author: Prakash Besra
